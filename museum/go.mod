abfbb17d5f988f7317a730e4f5b13fc855b1c57c
lasagna <arthurebenezer@aol.com>
2024-06-11 20:43:59 +0000

    feat: basic vanilla api

---
+++ museum/go.mod
@@ -0,0 +1,3 @@
+module github.com/ebarthur/golang/museum
+
+go 1.22.3
--- museum/main.go
+++ museum/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "GET" {
+		w.Write([]byte("Welcome to API 1.0.1"))
+	}
+}
+
 func main() {
+
+	server := http.NewServeMux()
+
+	server.HandleFunc("/hello", handleHello)
+
+	err := http.ListenAndServe(":3000", server)
+
+	if err != nil {
+		fmt.Println("Error while opening the server")
+	}
 
 }
